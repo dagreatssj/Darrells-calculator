f12d1a36dd760fcd34e8b0c630dad53cd91f91e8
darrell <Darrell.Calderon@qvc.com>
2014-07-16 11:30:13 -0700

    updated title

--- calculator.py
+++ calculator.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html", title='QVC Calculator')
+	return render_template("index.html", title='Darrell\'s Calculator')
