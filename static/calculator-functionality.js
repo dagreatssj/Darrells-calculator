3eb80fc29d6622a939b00753f783394d7deb5d7e
darrell <Darrell.Calderon@qvc.com>
2014-07-23 16:33:17 -0700

    some ui changes

--- static/calculator-functionality.js
+++ static/calculator-functionality.js
@@ -22,38 +22,6 @@ function doCalculation() {
 	$('#history').append("<p>" + entered + "=" + results + "</p>");
 
 	console.log(results);
-	/*var postfix = [];
-	var operands = [];
-	for (var i = 0; i < entered.length; i++) {
-		if ($.isNumeric(entered.charAt(i) && i == 0)) {
-			postfix.push(entered.charAt(i));
-		} else if($.isNumeric(entered.charAt(i))) { 
-			//postfix[i-1] += entered.charAt(i);
-		} else {
-			operands.push(entered.charAt(i));
-		}
-	}
-	
-	for (var i = 0; i < postfix.length; i++) {
-		console.log(postfix[i]);
-	}*/
-
-	/*var numbers = entered.match(/(\d+)/g);
-	for (var i = 0; i < numbers.length; i++) {
-		console.log(numbers[i]);
-	}
-
-	var operators = [];
-	for (var i=0; i < entered.length; i++) {
-		if (!$.isNumeric(entered.charAt(i))) {
-			operators.push(entered.charAt(i));
-		}
-	}
-
-	for (var i=0; i < operators.length; i++) {
-		console.log(operators[i]);
-	}*/
-	
 }
 
 function clearScreen() {
